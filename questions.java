c6a005a887ee6e08f6989e069920725b78e1100a
JefferyAlmond <jalmond@gmail.com>
2019-01-13 15:01:33 -0800

    Update questions.java

--- questions.java
+++ questions.java
@@ -5,21 +5,7 @@ import java.math.*;
 
 public class questions {
 
-	public static void main(String[] args) {
-		Node n1 = new Node(1);
-		Node n2 = new Node(2);
-		Node n3 = new Node(3);
-		Node n4 = new Node(4);
-		Node n5 = new Node(5);
-		Node n6 = new Node(6);
-		
-		n1.next = n2;
-		n2.next = n3;
-		n3.next = n4;
-		n4.next = n5;
-		n5.next = n3;
-		n6.next = n1;
-		
+	public static void main(String[] args) {	
 		
 	}
 	
